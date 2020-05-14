5dc01ceeee1340bf66f79ebda8ba62037587478f
gaurav <sharma02gaurav@gmail.com>
2020-05-14 22:34:34 +0530

    Bindings for JWT and custom user service

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,7 @@
+import {TokenService, UserService} from '@loopback/authentication';
 import {BindingKey} from '@loopback/core';
+import {User} from '../models';
+import {Credentials} from '../repositories';
 import {PasswordHasher} from '../services/hash.password.bcrypt';
 
 /**
@@ -7,4 +10,20 @@ import {PasswordHasher} from '../services/hash.password.bcrypt';
 export namespace PasswordHasherBindings {
   export const PASSWORD_HASHER = BindingKey.create<PasswordHasher>('services.hasher');
   export const ROUNDS = BindingKey.create<number>('services.hasher.round');
+}
+
+export namespace TokenServiceConstants {
+  export const TOKEN_SECRET_VALUE = process.env.secret ?? 'm@YTh3F0Rc3bEW!thY0u';
+  export const TOKEN_EXPIRES_IN_VALUE = '600';
+}
+
+// custom namespace bindings for authentication
+export namespace AuthTokenBindings {
+  export const TOKEN_SECRET = BindingKey.create<string>('auth.jwt.secret');
+  export const TOKEN_EXPIRES_IN = BindingKey.create<string>('auth.jwt.expiry');
+  export const TOKEN_SERVICE = BindingKey.create<TokenService>('auth.jwt.tokenservice');
+}
+
+export namespace UserServiceBindings {
+  export const USER_SERVICE = BindingKey.create<UserService<User, Credentials>>('services.user.service')
 }
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -4,6 +4,11 @@ import {HttpErrors} from '@loopback/rest';
 import {PostgresDataSource} from '../datasources';
 import {User, UserRelations} from '../models';
 
+export type Credentials = {
+  email: string,
+  password: string,
+}
+
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
@@ -30,5 +35,17 @@ export class UserRepository extends DefaultCrudRepository<
       throw new HttpErrors.UnprocessableEntity('Email is already taken');
     }
     return this.create(user);
+  }
+
+  /**
+   * fetch the user based on the passed credentials
+   * @param credential
+   */
+  async getUserByCredential(credentials: Credentials): Promise<User> {
+    const user = await this.findOne({where: {email: credentials.email}});
+    if (!user) {
+      throw new HttpErrors.Unauthorized('User not found');
+    }
+    return user;
   }
 }
